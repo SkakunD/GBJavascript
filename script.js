379ab174d38b1eb327be5f99eff594e0c780bb92
SkakunD <beenom314@gmail.com>
2021-02-27 01:09:20 +0300

    Upgrade and add coment in code

--- script.js
+++ script.js
@@ -1,105 +1,105 @@
-// const goods = [
-//   { title: "Shirt", price: 150 },
-//   { title: "Socks", price: 50 },
-//   { title: "Jacket", price: 350 },
-//   { title: "Shoes", price: 250 },
-//   { price: 250 }, // товар у которого отсутствет заголовок, но при это он прописан в значнии по умолчании функции renderGoodsItem
-//   { title: "Shoes" }, // аналогично товар без цены
-// ];
-
-let itemsList = document.querySelector(".goods-list"); // объявляем переменную и присваиваем блок куда будем рендерить товары, чтобы повторно не вызывать querySelector
-
-class GoodsItem {
-  // класс одного товара и его рендер
-  constructor(title, price) {
-    this.title = title;
-    this.price = price;
-  }
-  render() {
-    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p><input type='button' class='addToBasket' value='Click to basket'></div>`;
-  }
-}
-
 class GoodsList {
+  // объявляем класс списка
   // класс списка товаров и рендер списка на основое созданного класса от одного товара
-  constructor() {
-    this.goods = [];
-  }
-  fetch() {
-    this.goods = [
-      { title: "Shirt", price: 150 },
-      { title: "Socks", price: 50 },
-      { title: "Jacket", price: 350 },
-      { title: "Shoes", price: 250 },
-      { price: 250 },
-      { title: "Shoes" },
-    ];
-  }
-  render() {
-    let listHtml = "";
-    this.goods.forEach((item) => {
-      const goodItem = new GoodsItem(item.title, item.price);
-      listHtml += goodItem.render();
+  goods = [];
+  filteredGoods = [];
+
+  __getGoodsItemTemplate = ({ productName, price, id }) => {
+    // верстка отдельного элемента списка
+    return `<div>
+      <h2>${productName}</h2>
+      <h5>${price}</h5>
+      <button data-goods-id="${id}">Add to cart</button>
+      <hr/>
+    </div>`;
+  };
+
+  handleChange = (event) => {
+    //метод сортировки
+    this.filteredGoods = this.goods.filter((item) => {
+      // списку присваиваем исходный список с методом фильтр
+      const v = event.target.value.toLowerCase(); // создалем константу в которой записывает строку приведенную к нижнему регистру
+      return item.productName.toLowerCase().includes(v); // возвращем находим знакчние в которому символы приведенные к нижнему регистру имеют включение константы
     });
-    itemsList.innerHTML = listHtml;
-  }
-  countPrice() {
-    //метод подсчета суммы товаров на странице - 2 задание
-    let itemsPrice = 0;
-    this.goods.forEach((item) => {
-      if (item.price !== undefined) {
-        itemsPrice += item.price;
-      }
+    console.log(this.filteredGoods);
+    this.render(".goods-list"); // запускаем метод рендер с аргументм обновленного списка
+  };
+
+  init(url) {
+    // мотед работы строки поиска
+    this.input = document.querySelector("[data-id=search]"); // получаем введенный символы
+    this.input.addEventListener("input", this.handleChange); // добавляем слушаетеля на инпут и в воде колбека запуск фукнции фильтрации
+    this.getGoods(url).then(() => {
+      this.render(".goods-list");
     });
-    console.log(itemsPrice);
+  }
+
+  getGoods(url) {
+    // метод для запроса списка по адресу
+    return fetch(url)
+      .then((r) => r.json())
+      .then((r) => {
+        this.goods = r;
+        this.filteredGoods = this.goods;
+      });
+  }
+
+  render(selector = ".goods-list") {
+    // метод рендера списка
+    const goodsTemplates = this.filteredGoods
+      .map((item) => this.__getGoodsItemTemplate(item))
+      .join("");
+    const wrapper = document.querySelector(selector);
+    wrapper.innerHTML = goodsTemplates;
+    wrapper.querySelectorAll("[data-goods-id]").forEach((i) => {
+      // навешиваем слушаетля на клик для добавления в корзину
+      i.addEventListener("click", () => {
+        console.log(cart);
+        const id = i.getAttribute("data-goods-id");
+        const item = this.goods.find((goodsItem) => goodsItem.id === id);
+        cart.addToBasket(item);
+      });
+    });
   }
 }
 
 class Basket {
-  constructor() {
-    this.backetItems = [];
-    this.totalPrice = "";
-    this.amountItems = "";
+  backetItems = [];
+
+  getItems() {
+    return this.backetItems;
   }
-  countPrice() {
-    // метод подсчитывающий сумму корзины
-    let count = "";
-    this.backetItems.forEach((item) => {
-      if (item.price !== undefined) {
-        count += item.price;
-      }
-    });
-    this.totalPrice = count;
+
+  addToBasket(item) {
+    this.backetItems.push(item);
+    this.render();
   }
-  countItems() {
-    // метод подсчитывающий общее количество товаров
-    this.amountItems = this.backetItems.length;
+
+  removeFromBasket(id) {
+    this.backetItems = this.backetItems.filter((item) => item.id !== id);
+    this.render();
+  }
+
+  __getGoodsItemTemplate = ({ productName, price, id }) => {
+    return `<div>
+      <h5>${productName}</h5>
+      <h3>${price}</h3>
+      <button data-goods-id="${id}">Remove from cart</button>
+      <hr/>
+    </div>`;
+  };
+
+  render() {
+    const goodsTemplates = this.backetItems
+      .map((item) => this.__getGoodsItemTemplate(item))
+      .join("");
+    const wrapper = document.querySelector("[data-id=cart]");
+    wrapper.innerHTML = goodsTemplates;
   }
 }
 
-class basketItem extends GoodsItem {
-  constructor() {
-    super(title, price);
-    this.countItem = "";
-  }
-  deleteItem() {
-    // метод удаляющий товар из корзины
-  }
-}
+const cart = new Basket();
 
-// const renderGoodsItem = (title = "Name item", price = 0) => {
-//   // можно упростить убрав return  и { } но тогда код будет менее читабельный
-//   return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-// };
-
-// const renderGoodsList = (list) => {
-//   let goodsList = list.map((item) => renderGoodsItem(item.title, item.price));
-//   //   console.log(goodsList);
-//   itemsList.innerHTML = goodsList.join(""); // запятые убрали методом массива join так как результат работы map массив с версткой и запятыми, которые innerHTML не обрабатывает а выводит как новый блок
-// };
-
-// renderGoodsList(goods);
-const newList = new GoodsList();
-newList.fetch();
-newList.render();
-newList.countPrice();
+const url =
+  "https://mock-api-builder.vercel.app/api/schema/get/602c166a89c4a60009ef7046";
+const list = new GoodsList().init(url);
