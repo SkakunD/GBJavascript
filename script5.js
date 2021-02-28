0f44ed596a7a6520f98d6355f3b2a2b1942af3ac
SkakunD <beenom314@gmail.com>
2021-02-28 17:35:51 +0300

    add Vue functional, add method addToBasket

---
+++ script5.js
@@ -0,0 +1,572 @@
+// let listItem = [
+//   {
+//     productName: "Fantastic Fresh Hat",
+//     price: "779.00$",
+//     id: "63eb1348-35a6-421c-9f49-dee3d4fd1f9a",
+//   },
+//   {
+//     productName: "Rustic Granite Sausages",
+//     price: "755.00$",
+//     id: "d5acfd6d-cca3-4ac0-8a4f-b51ed411a633",
+//   },
+//   {
+//     productName: "Tasty Cotton Cheese",
+//     price: "199.00$",
+//     id: "58220a84-794f-409a-af90-02907eeb5440",
+//   },
+//   {
+//     productName: "Fantastic Granite Chips",
+//     price: "983.00$",
+//     id: "1ba24d95-4635-4791-ad94-ac4312c8e27e",
+//   },
+//   {
+//     productName: "Practical Wooden Salad",
+//     price: "553.00$",
+//     id: "73b974b5-302b-4c4c-b694-cb19abe94c45",
+//   },
+//   {
+//     productName: "Tasty Metal Towels",
+//     price: "206.00$",
+//     id: "37a60115-873f-457f-868a-7ef59b44edf7",
+//   },
+//   {
+//     productName: "Refined Wooden Keyboard",
+//     price: "584.00$",
+//     id: "b6ce542a-0574-4ef0-8ba2-f26c81d4ede5",
+//   },
+//   {
+//     productName: "Refined Rubber Towels",
+//     price: "286.00$",
+//     id: "37707feb-63d7-44c2-aed6-54deba7d749c",
+//   },
+//   {
+//     productName: "Intelligent Frozen Shoes",
+//     price: "818.00$",
+//     id: "06446806-c770-4ce6-850f-605a8cc385bd",
+//   },
+//   {
+//     productName: "Awesome Wooden Salad",
+//     price: "178.00$",
+//     id: "87a6d1c3-1809-401d-8ac2-8ef42257cbaf",
+//   },
+//   {
+//     productName: "Gorgeous Concrete Cheese",
+//     price: "354.00$",
+//     id: "100cba1f-771d-4e46-bebe-26e8b9c1ef25",
+//   },
+//   {
+//     productName: "Rustic Cotton Computer",
+//     price: "467.00$",
+//     id: "2bad33ac-3d70-4357-bd9c-abf3940d2d2f",
+//   },
+//   {
+//     productName: "Rustic Cotton Chair",
+//     price: "331.00$",
+//     id: "199fc8de-10ae-4374-8086-ac7a646a4d6a",
+//   },
+//   {
+//     productName: "Incredible Steel Sausages",
+//     price: "638.00$",
+//     id: "f9f83d6c-8fc4-4b68-b537-08ab9e75e835",
+//   },
+//   {
+//     productName: "Practical Steel Soap",
+//     price: "425.00$",
+//     id: "8e6c69ad-550e-4322-bf03-0d559cc65de8",
+//   },
+//   {
+//     productName: "Sleek Wooden Sausages",
+//     price: "158.00$",
+//     id: "59d73797-3532-427f-a36d-1a0b21d0b180",
+//   },
+//   {
+//     productName: "Fantastic Steel Salad",
+//     price: "159.00$",
+//     id: "142ae039-12a2-4b2f-95dd-dcdf4d0753f6",
+//   },
+//   {
+//     productName: "Awesome Steel Mouse",
+//     price: "407.00$",
+//     id: "12ff3762-6785-4ea6-a7e2-55d0c3b43321",
+//   },
+//   {
+//     productName: "Rustic Wooden Shirt",
+//     price: "592.00$",
+//     id: "8339de7d-ce39-4510-aa25-a861556eab86",
+//   },
+//   {
+//     productName: "Awesome Soft Sausages",
+//     price: "841.00$",
+//     id: "ce46339c-908e-4099-9f4f-0976b8337777",
+//   },
+//   {
+//     productName: "Handmade Wooden Table",
+//     price: "67.00$",
+//     id: "c67d9e78-0300-416d-a6a7-e969f01ff31c",
+//   },
+//   {
+//     productName: "Generic Metal Bacon",
+//     price: "410.00$",
+//     id: "10651818-3924-4164-9326-efc65392edd4",
+//   },
+//   {
+//     productName: "Practical Concrete Mouse",
+//     price: "904.00$",
+//     id: "cc86926e-6688-4244-8f98-d507a04f6f8e",
+//   },
+//   {
+//     productName: "Fantastic Soft Shirt",
+//     price: "215.00$",
+//     id: "023f6b16-2323-47ed-88d0-40caec930925",
+//   },
+//   {
+//     productName: "Ergonomic Granite Car",
+//     price: "530.00$",
+//     id: "6ba4a8a3-73bb-4543-935d-8d76c484a4d3",
+//   },
+//   {
+//     productName: "Handmade Granite Hat",
+//     price: "221.00$",
+//     id: "2308eb4e-5f06-4124-aa37-124cc3769c75",
+//   },
+//   {
+//     productName: "Handcrafted Steel Fish",
+//     price: "534.00$",
+//     id: "e4cc0620-a75a-42db-b696-ce6d175eff6a",
+//   },
+//   {
+//     productName: "Incredible Soft Car",
+//     price: "92.00$",
+//     id: "fc24dcfd-e189-47e2-9518-082877c4dfaa",
+//   },
+//   {
+//     productName: "Awesome Plastic Chicken",
+//     price: "969.00$",
+//     id: "b1d66fc7-7908-48fc-9a42-d5b45501648e",
+//   },
+//   {
+//     productName: "Licensed Steel Soap",
+//     price: "879.00$",
+//     id: "f80a0974-9c25-48be-9155-cc62d2f6b2d5",
+//   },
+//   {
+//     productName: "Licensed Metal Keyboard",
+//     price: "712.00$",
+//     id: "c4140243-4d59-4a75-bae8-917e8de9b88a",
+//   },
+//   {
+//     productName: "Handmade Frozen Chicken",
+//     price: "761.00$",
+//     id: "4839b1fd-ddf1-4ce7-8149-88997fcc04a9",
+//   },
+//   {
+//     productName: "Sleek Frozen Bacon",
+//     price: "182.00$",
+//     id: "7a4c4968-d075-4bed-96aa-65db424aaec8",
+//   },
+//   {
+//     productName: "Unbranded Granite Shirt",
+//     price: "439.00$",
+//     id: "42989716-6bcf-4977-b718-fe8a35c602be",
+//   },
+//   {
+//     productName: "Intelligent Plastic Car",
+//     price: "977.00$",
+//     id: "1f83a9d0-bbc2-46b6-9b06-946c543c6f3e",
+//   },
+//   {
+//     productName: "Fantastic Steel Chicken",
+//     price: "888.00$",
+//     id: "e67d294c-4005-4e20-ba57-4ae8c8f3eaed",
+//   },
+//   {
+//     productName: "Unbranded Concrete Table",
+//     price: "703.00$",
+//     id: "8fd2d155-59a6-424e-9ea2-0a1922514b38",
+//   },
+//   {
+//     productName: "Refined Rubber Chair",
+//     price: "684.00$",
+//     id: "b7c439e5-b6f8-4c56-9a35-f78b4eca84d0",
+//   },
+//   {
+//     productName: "Gorgeous Cotton Sausages",
+//     price: "401.00$",
+//     id: "475b484e-de21-472a-8ea4-60a336fdcb6c",
+//   },
+//   {
+//     productName: "Generic Frozen Fish",
+//     price: "880.00$",
+//     id: "e1caa01f-7990-4121-a73f-5ebcc105aac4",
+//   },
+//   {
+//     productName: "Licensed Frozen Chips",
+//     price: "528.00$",
+//     id: "5f06bd9a-1a78-4d58-84b3-267ca0c1da96",
+//   },
+//   {
+//     productName: "Ergonomic Frozen Hat",
+//     price: "263.00$",
+//     id: "83d095df-2ab9-44f8-a0d6-1db43d3c380b",
+//   },
+//   {
+//     productName: "Sleek Frozen Shoes",
+//     price: "208.00$",
+//     id: "606b8177-6491-4cb1-92f2-a81ba249ccbb",
+//   },
+//   {
+//     productName: "Gorgeous Rubber Bacon",
+//     price: "701.00$",
+//     id: "af74903d-87eb-4045-80a8-1f03ab5644df",
+//   },
+//   {
+//     productName: "Sleek Soft Salad",
+//     price: "555.00$",
+//     id: "70f0c282-f450-4049-a7eb-6b4cb78b0dfc",
+//   },
+//   {
+//     productName: "Gorgeous Granite Chips",
+//     price: "349.00$",
+//     id: "a867d291-3bdf-42da-98d9-f0e31ce647e5",
+//   },
+//   {
+//     productName: "Practical Metal Fish",
+//     price: "458.00$",
+//     id: "27b2d230-8efd-48ca-b41c-18b94e419553",
+//   },
+//   {
+//     productName: "Gorgeous Metal Gloves",
+//     price: "568.00$",
+//     id: "cb99cd6b-7661-4b5f-bd70-c1936bab08f9",
+//   },
+//   {
+//     productName: "Generic Fresh Soap",
+//     price: "787.00$",
+//     id: "d1b5f2e0-7dea-4c9c-9841-dba8613a9623",
+//   },
+//   {
+//     productName: "Handcrafted Metal Bike",
+//     price: "699.00$",
+//     id: "cdc80261-28d6-4018-85bf-2e04fb12a305",
+//   },
+//   {
+//     productName: "Rustic Fresh Car",
+//     price: "280.00$",
+//     id: "d272551a-ab14-40f8-b9b2-426e53bc1404",
+//   },
+//   {
+//     productName: "Small Rubber Pizza",
+//     price: "77.00$",
+//     id: "ba98ee5c-fbcf-49d8-8192-6ffead0b4603",
+//   },
+//   {
+//     productName: "Fantastic Concrete Hat",
+//     price: "662.00$",
+//     id: "c4ea4eae-244c-4a3e-ba0e-cb9d782e3326",
+//   },
+//   {
+//     productName: "Practical Rubber Hat",
+//     price: "608.00$",
+//     id: "756d3e88-7704-4e0d-9d74-473c6b623865",
+//   },
+//   {
+//     productName: "Ergonomic Rubber Chair",
+//     price: "715.00$",
+//     id: "5c814c91-0338-4e9c-9d45-683e3eb0f5ca",
+//   },
+//   {
+//     productName: "Ergonomic Cotton Hat",
+//     price: "791.00$",
+//     id: "ca5194de-e079-4425-87c9-73a239f74e10",
+//   },
+//   {
+//     productName: "Generic Frozen Tuna",
+//     price: "52.00$",
+//     id: "cc2a70e3-77f1-4599-95a6-ffa8796f0541",
+//   },
+//   {
+//     productName: "Rustic Steel Soap",
+//     price: "838.00$",
+//     id: "32f80f72-3ac1-4862-a35f-e52918f7646a",
+//   },
+//   {
+//     productName: "Fantastic Soft Gloves",
+//     price: "144.00$",
+//     id: "1f8500d0-bb07-4635-a1b4-c5fff3555058",
+//   },
+//   {
+//     productName: "Intelligent Steel Ball",
+//     price: "273.00$",
+//     id: "da52feae-dc47-413b-baf4-fe04be0766c6",
+//   },
+//   {
+//     productName: "Awesome Plastic Salad",
+//     price: "96.00$",
+//     id: "ceb50342-0240-4a9f-9ada-4967d385b9ad",
+//   },
+//   {
+//     productName: "Ergonomic Metal Keyboard",
+//     price: "949.00$",
+//     id: "4337359d-08d6-4d2a-8999-fb9b5b50ec66",
+//   },
+//   {
+//     productName: "Fantastic Metal Bike",
+//     price: "887.00$",
+//     id: "7f08e824-73d8-4ca8-a2c4-ee01976fb7d7",
+//   },
+//   {
+//     productName: "Licensed Fresh Shirt",
+//     price: "471.00$",
+//     id: "f194bb75-a895-44e2-a9dd-db27d372448c",
+//   },
+//   {
+//     productName: "Sleek Metal Gloves",
+//     price: "58.00$",
+//     id: "9180942b-1ace-45f6-b23a-b307049cc9d0",
+//   },
+//   {
+//     productName: "Licensed Soft Shoes",
+//     price: "106.00$",
+//     id: "fa562d9f-e807-4dec-8363-8f3ae9181b51",
+//   },
+//   {
+//     productName: "Generic Steel Fish",
+//     price: "237.00$",
+//     id: "8f258666-0574-4152-a6a9-619cdb5af09b",
+//   },
+//   {
+//     productName: "Awesome Granite Bacon",
+//     price: "113.00$",
+//     id: "55c4d3ed-c0e1-41c6-9ac4-4616ccc960d1",
+//   },
+//   {
+//     productName: "Awesome Plastic Ball",
+//     price: "440.00$",
+//     id: "41b224d1-f18d-4380-8fa6-949d44bfa6ae",
+//   },
+//   {
+//     productName: "Awesome Soft Gloves",
+//     price: "423.00$",
+//     id: "29599694-f477-4709-8854-89857b183001",
+//   },
+//   {
+//     productName: "Tasty Frozen Fish",
+//     price: "16.00$",
+//     id: "5b6e2e3d-e847-4abb-a760-4e23197ba6af",
+//   },
+//   {
+//     productName: "Rustic Fresh Bike",
+//     price: "915.00$",
+//     id: "bafa5629-90e3-4c80-a3da-54522f936d85",
+//   },
+//   {
+//     productName: "Tasty Soft Shirt",
+//     price: "392.00$",
+//     id: "00ed2f50-e56a-43b6-9dde-db5d9794fcd8",
+//   },
+//   {
+//     productName: "Licensed Fresh Gloves",
+//     price: "413.00$",
+//     id: "af686c73-c897-4284-8157-1d7438e4e0ed",
+//   },
+//   {
+//     productName: "Handcrafted Steel Keyboard",
+//     price: "26.00$",
+//     id: "01f402e2-8ec0-484f-8596-3eaea304fc77",
+//   },
+//   {
+//     productName: "Licensed Granite Hat",
+//     price: "747.00$",
+//     id: "ea545b43-e9b9-4e27-bf5e-45af867bbe6b",
+//   },
+//   {
+//     productName: "Tasty Soft Tuna",
+//     price: "388.00$",
+//     id: "c66cee2c-aaad-4b49-96a9-65245e9db16d",
+//   },
+//   {
+//     productName: "Practical Steel Towels",
+//     price: "625.00$",
+//     id: "7939af64-339b-4586-ae13-0b1ecfd48bb6",
+//   },
+//   {
+//     productName: "Awesome Frozen Sausages",
+//     price: "433.00$",
+//     id: "eab374de-85b3-4a38-a365-aabd5ba7f8e6",
+//   },
+//   {
+//     productName: "Generic Granite Ball",
+//     price: "253.00$",
+//     id: "ba7e4663-6b44-4dec-a528-1b88b06a55f8",
+//   },
+//   {
+//     productName: "Refined Frozen Fish",
+//     price: "1.00$",
+//     id: "17fd916c-621d-43e4-96a9-a0748e109e7a",
+//   },
+//   {
+//     productName: "Handmade Plastic Soap",
+//     price: "254.00$",
+//     id: "ae324463-22ba-4e55-b14d-eb1ea6f87b04",
+//   },
+//   {
+//     productName: "Handcrafted Cotton Gloves",
+//     price: "467.00$",
+//     id: "ec437ae5-f585-449c-b494-a065898a1c51",
+//   },
+//   {
+//     productName: "Generic Granite Table",
+//     price: "919.00$",
+//     id: "20e0703d-0fa8-47f6-8638-62209b47e50a",
+//   },
+//   {
+//     productName: "Ergonomic Metal Keyboard",
+//     price: "725.00$",
+//     id: "9d19fc83-e639-4f36-b25b-4ccb069873aa",
+//   },
+//   {
+//     productName: "Rustic Rubber Hat",
+//     price: "987.00$",
+//     id: "bdbd3a93-d339-4b44-99bb-bcf30256646c",
+//   },
+//   {
+//     productName: "Incredible Frozen Bacon",
+//     price: "469.00$",
+//     id: "7220faf6-31fe-4b83-bc56-06f2277e7aeb",
+//   },
+//   {
+//     productName: "Refined Wooden Chips",
+//     price: "584.00$",
+//     id: "69c0f186-468a-40d5-b11f-b4b9603b657a",
+//   },
+//   {
+//     productName: "Gorgeous Cotton Pants",
+//     price: "283.00$",
+//     id: "5b7691ad-14eb-457b-b881-1da556a7f173",
+//   },
+//   {
+//     productName: "Small Wooden Shoes",
+//     price: "579.00$",
+//     id: "00e8c632-238b-4cfd-93b1-510cc6fd899d",
+//   },
+//   {
+//     productName: "Small Cotton Ball",
+//     price: "182.00$",
+//     id: "e64857f6-2f7c-486f-8b35-d7c90e621014",
+//   },
+//   {
+//     productName: "Refined Rubber Shirt",
+//     price: "70.00$",
+//     id: "d4576551-a7f2-42ac-97d5-0b81f24378c8",
+//   },
+//   {
+//     productName: "Incredible Metal Shoes",
+//     price: "411.00$",
+//     id: "806ba156-6312-4fd5-9a42-c7e2c15546ea",
+//   },
+//   {
+//     productName: "Tasty Rubber Towels",
+//     price: "470.00$",
+//     id: "41d86500-88c3-4514-9c76-ab8006a2ab05",
+//   },
+//   {
+//     productName: "Refined Rubber Chair",
+//     price: "665.00$",
+//     id: "7485ce83-4ebc-4953-9250-4b489289831d",
+//   },
+//   {
+//     productName: "Small Steel Gloves",
+//     price: "798.00$",
+//     id: "dbbfa867-116f-4d09-babd-a2e25add682f",
+//   },
+//   {
+//     productName: "Fantastic Wooden Shirt",
+//     price: "593.00$",
+//     id: "b16675b7-2878-4624-8858-5a14ba53ee66",
+//   },
+//   {
+//     productName: "Rustic Wooden Mouse",
+//     price: "564.00$",
+//     id: "c1e4c671-0321-44a1-9d3b-2049957fe0a5",
+//   },
+//   {
+//     productName: "Rustic Granite Computer",
+//     price: "718.00$",
+//     id: "66ae776c-61b0-4e13-b17b-a550d5f05b79",
+//   },
+//   {
+//     productName: "Practical Wooden Shoes",
+//     price: "575.00$",
+//     id: "a771728d-3a91-4ac6-8261-311828ae910a",
+//   },
+// ];
+
+// let app = new Vue({
+//   el: "#root",
+//   data: {
+//     message: "My first project",
+//     // datetime: new Date().toISOString(),
+//     listItems: listItem,
+//   },
+//   methods: {
+//     handleClick(id) {
+//       console.log(id);
+//     },
+//   },
+//   computed: {
+//     notEmpty() {
+//       return this.message.trim().length !== 0;
+//     },
+//   },
+//   mounted() {
+//     console.log("work");
+//   },
+// });
+
+let BASE_URL = "https://mock-api-builder.vercel.app/api/schema/get";
+
+let app = new Vue({
+  el: "#root",
+  data: {
+    goods: [],
+    // filteredGoods: [],
+    searchGoods: "",
+    errorMessage: "",
+    cart: [],
+    // count: 0,
+  },
+  methods: {
+    getGoods() {
+      fetch(`${BASE_URL}/602c166a89c4a60009ef7046`)
+        .then((r) => r.json())
+        .then((r) => {
+          this.goods = r;
+          this.filteredGoods = this.goods;
+        })
+        .catch((e) => {
+          this.errorMessage = e;
+          alert("List is empty");
+        });
+    },
+    addToBasket(arg) {
+      let tempItem = this.goods.filter((item) => item.id.includes(arg));
+      // console.log(tempItem);
+      if (this.cart.includes(tempItem[0])) {
+        // this.count += 1;
+      } else {
+        this.cart.push(tempItem[0]);
+      }
+      console.log(this.cart);
+    },
+  },
+  computed: {
+    filteredGoods() {
+      return this.goods.filter((item) =>
+        item.productName.toLowerCase().includes(this.searchGoods)
+      );
+    },
+  },
+  mounted() {
+    this.getGoods();
+  },
+});
